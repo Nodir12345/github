fee07000e63e6f19312a45acb9b55ceb0c88fb82
Nodir12345 <nodirxaydarov911@gmail.com>
2023-02-11 16:48:18 +0500

    sdc

--- src/pages/packeges/packages.jsx
+++ src/pages/packeges/packages.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
-import "./packages.scss"
-import { Contact } from '../../components/contact/contact.jsx'
+import React from "react";
+import "./packages.scss";
+import { Contact } from "../../components/contact/contact.jsx";
 export const Packages = () => {
   return (
-    <div className='container package'>
-<Contact></Contact>
-    <div className='package_box'>
-      
+    <div className="container package">
+      <Contact></Contact>
+      <div className="package_box"></div>
     </div>
-    </div>
-  )
-}
-
-
+  );
+};
